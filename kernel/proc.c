0436ea1f8de2af24a12935a44ffe31e64e8af33a
afschy <1905081@ugrad.cse.buet.ac.bd>
2023-07-22 15:15:19 +0600

    fixed formatting and error handling

--- kernel/proc.c
+++ kernel/proc.c
@@ -576,7 +576,7 @@ scheduler(void)
         p->state = RUNNING;
         p->slices_given = TIME_LIMIT_1;
         p->slices_used = 0;
-        p->tickets_curr--;
+        p->tickets_curr -= TIME_LIMIT_1;
         c->proc = p;
         swtch(&c->context, &p->context);
 
